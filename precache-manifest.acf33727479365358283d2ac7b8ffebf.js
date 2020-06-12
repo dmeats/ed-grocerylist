3cf94c000ba55dbadeb082b096a7633dafae7bd3
dmeats <djmmm@hotmail.com>
2020-06-12 15:01:29 -0400

    Updates

---
+++ precache-manifest.acf33727479365358283d2ac7b8ffebf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63caf3adc7605eeb31726b526420dbd1",
+    "url": "/ed-grocerylist/index.html"
+  },
+  {
+    "revision": "e7b1568f4b4695bbf701",
+    "url": "/ed-grocerylist/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "a05051d9f88fe81c212d",
+    "url": "/ed-grocerylist/static/css/main.14fcbcf3.chunk.css"
+  },
+  {
+    "revision": "e7b1568f4b4695bbf701",
+    "url": "/ed-grocerylist/static/js/2.7174ccde.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/ed-grocerylist/static/js/2.7174ccde.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a05051d9f88fe81c212d",
+    "url": "/ed-grocerylist/static/js/main.aae8bca3.chunk.js"
+  },
+  {
+    "revision": "16d8220fc6c7dd963078",
+    "url": "/ed-grocerylist/static/js/runtime-main.2e3c9d24.js"
+  }
+]);
